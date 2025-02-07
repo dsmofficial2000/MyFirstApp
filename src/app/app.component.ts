972bb85f523e0478b4b23d7c2e29debb90dee4d8
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-08 03:18:03 +0530

    Displaying Message Dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name : string = "";
+  title = 'Durgesh';
+  msg = 'Hello , Welcome to app component `page';
+
+  a = 10;
+  b = 20;
+  ans = this.a + this.b;
 }
