5e23039925262c7cf1890a333b1d181e55ebc3bc
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-08 12:01:29 +0530

    property binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
 export class TestComponent {
+  name : string = "";
+  title = 'Durgesh';
+  msg = 'Hello , Welcome to app component `page';
+  inputVal = 'ABC Test';
+  isDisabled = true;
 
+  a = 10;
+  b = 20;
+  ans = this.a + this.b;
 }
