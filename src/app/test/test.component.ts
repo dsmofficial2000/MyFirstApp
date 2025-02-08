c3c81d6f612ba370ccc1c9088d86de21e5c9e3d2
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-08 13:42:45 +0530

    keyboard event

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,24 +10,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  name : string = "";
-  title = 'Durgesh';
-  msg = 'Hello , Welcome to app component `page';
-  inputVal = 'ABC Test';
-  isDisabled = true;
-  imgSrc = 'assets/images.png';
-
-  a = 10;
-  b = 20;
-  ans = this.a + this.b;
-
-  display(msg:string){
-    alert(msg);
+  keyPress() {
+    console.log('Keypress event is triggered.');
   }
+  // name : string = "";
+  // title = 'Durgesh';
+  // msg = 'Hello , Welcome to app component `page';
+  // inputVal = 'ABC Test';
+  // isDisabled = true;
+  // imgSrc = 'assets/images.png';
 
-  //Define one variable
-  counter = 0;
-  Increment() {
-    this.counter++;
-  }
+  // a = 10;
+  // b = 20;
+  // ans = this.a + this.b;
+
+  // display(msg:string){
+  //   alert(msg);
+  // }
+
+  // //Define one variable
+  // counter = 0;
+  // Increment() {
+  //   this.counter++;
+  // }
 }
