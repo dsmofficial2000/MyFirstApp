9e579c6511c5fffb297722238ca764cb4f510b56
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-09 16:27:15 +0530

    *ngSwitch Statement

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -17,18 +17,12 @@ interface emplist{
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  
-  displayElement: boolean = false;
 
-  togglelist() {
-    this.displayElement = !this.displayElement;
+  age :number = 0;
+  isEligible : boolean = false;
+  
+  checkAgeEligibility() {
+    this.isEligible = this.age >= 18;
   }
-  employees = <emplist[]>([
-    { id: 1, name: 'EmployeeA', role: 'Admin' },
-    { id: 2, name: 'EmployeeB', role: 'HR' },
-    { id: 3, name: 'EmployeeC', role: 'Marketing' },
-    { id: 4, name: 'EmployeeD', role: 'Developer' },
-    { id: 5, name: 'EmployeeE', role: 'Marketing' },
-  ]);
   
 }
