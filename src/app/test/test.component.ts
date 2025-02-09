14698662b07ecbe4442351477b2faa377a57b132
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-09 14:30:21 +0530

    Structural Directives (*ngIf)

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+// Angular 17
+interface emplist{
+  id: number;
+  name: string;
+  role: string;
+}
+
 @Component({
   selector: 'app-test',
   standalone: true,
@@ -10,9 +17,28 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  count = 0;
+  display: boolean = false;
 
-  counter(type:string) {
-    type==='incr'? this.count++ : this.count--;
+  displayelement: boolean = false;
+
+  num: number = -20;
+
+  show() {
+    this.display = !this.display;
   }
+
+  // Angular 17
+  showEmpList : boolean = false;
+  employees = <emplist[]>([
+    { id: 1, name: 'EmployeeA', role: 'Admin' },
+    { id: 2, name: 'EmployeeB', role: 'HR' },
+    { id: 3, name: 'EmployeeC', role: 'Marketing' },
+    { id: 4, name: 'EmployeeD', role: 'Developer' },
+    { id: 5, name: 'EmployeeE', role: 'Marketing' },
+  ]);
+
+  ToggleEmployee() {
+    this.showEmpList = !this.showEmpList;
+  }
+  
 }
