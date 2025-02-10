0e2624c2c911fb62e1f6c6576ffb8e75edc10182
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-10 23:10:10 +0530

    NG17-Control Flow Statements (@switch

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 // Angular 17
-interface emplist{
+interface Employee{
   id: number;
   name: string;
   role: string;
@@ -18,6 +18,19 @@ interface emplist{
 })
 export class TestComponent {
 
-  updateStyles = 'updateStyles';
+  employeeRole: string = '';
+
+  employees: Employee[] = [
+    { id: 1, name: 'EmployeeA', role: 'Admin' },
+    { id: 2, name: 'EmployeeB', role: 'HR' },
+    { id: 3, name: 'EmployeeC', role: 'Marketing' },
+    { id: 4, name: 'EmployeeD', role: 'Developer' },
+    { id: 5, name: 'EmployeeE', role: 'Marketing' },
+    { id: 6, name: 'EmployeeF', role: 'Marketing' },
+    { id: 7, name: 'EmployeeG', role: 'Developer' },
+    { id: 8, name: 'EmployeeH', role: 'Developer' },
+    { id: 9, name: 'EmployeeI', role: 'Analyst' },
+    { id: 10, name: 'EmployeeJ', role: 'Analyst' },
+  ];
   
 }
