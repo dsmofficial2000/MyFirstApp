9a947f46db930dc7944631d86b8ddc02a1030550
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-10 23:47:17 +0530

    ngClass lightMode / DarkMode

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -17,20 +17,11 @@ interface Employee{
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-
-  employeeRole: string = '';
-
-  employees: Employee[] = [
-    { id: 1, name: 'EmployeeA', role: 'Admin' },
-    { id: 2, name: 'EmployeeB', role: 'HR' },
-    { id: 3, name: 'EmployeeC', role: 'Marketing' },
-    { id: 4, name: 'EmployeeD', role: 'Developer' },
-    { id: 5, name: 'EmployeeE', role: 'Marketing' },
-    { id: 6, name: 'EmployeeF', role: 'Marketing' },
-    { id: 7, name: 'EmployeeG', role: 'Developer' },
-    { id: 8, name: 'EmployeeH', role: 'Developer' },
-    { id: 9, name: 'EmployeeI', role: 'Analyst' },
-    { id: 10, name: 'EmployeeJ', role: 'Analyst' },
-  ];
+  
+  colorMode = 'lightMode';
+  
+  toggleMode() {
+    this.colorMode = this.colorMode === 'lightMode' ? 'darkMode' : 'lightMode';
+  }
   
 }
