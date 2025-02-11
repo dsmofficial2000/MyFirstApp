becaa9eb32f321c180d955f637b9f8325b75333c
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-11 21:59:27 +0530

    Grouping element with ng-container and ng-template

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,13 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-// Angular 17
-interface Employee{
-  id: number;
-  name: string;
-  role: string;
-}
-
 @Component({
   selector: 'app-test',
   standalone: true,
@@ -17,7 +10,5 @@ interface Employee{
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  colorval = 'navy';
-  fontSize = '30px';
-  isItalic = 'italic';
+  isAuthorized : boolean = false;
 }
