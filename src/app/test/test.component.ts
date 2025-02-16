53134722f44fdf8c947ded6701d20aaa3e5df9f9
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-16 11:11:22 +0530

    blank N/A

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,26 +10,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-  nm = '';
-  em = '';
-  ph = '';
-  add = '';
-  submitted : boolean = false;
-  showHeading : boolean = true;
-  qualification = [{school: '', degree: '', year: ''}];
-
-  addQualification(){
-    this.qualification.push({school: '', degree: '', year:''});
-  }
-
-  submit(){
-    this.submitted = true;
-    this.showHeading = false;
-  }
-
-  edit(){
-    this.submitted = false;
-    this.showHeading = true;
-  }
+  
 
 }
