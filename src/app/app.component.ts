92f5d92e96387591c84986fe56ff59fad0dca27b
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-22 14:05:24 +0530

    pipes example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { AfterViewInit, Component, ElementRef, HostBinding, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+interface Employee {
+  name: string;
+  state: string;
+  salary: number;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,7 +17,14 @@ export class AppComponent {
   Showdate: Date = new Date();
 
   items = ['ABC', 'DEF', 'GHI', 'JKL'];
-  employees = [
+  employees: {name: string, state: string, salary: number }[] =[
+    {name: 'jason smith', state: 'california', salary: 50000},
+    {name: 'anderson brooke', state: 'new york', salary: 75000},
+    {name: 'alex costa', state: 'texas', salary: 60000},
+    {name: 'sean parker', state: 'pennsylvania', salary: 85000},
+  ];
+
+  employees1: Employee[] =[
     {name: 'jason smith', state: 'california', salary: 50000},
     {name: 'anderson brooke', state: 'new york', salary: 75000},
     {name: 'alex costa', state: 'texas', salary: 60000},
