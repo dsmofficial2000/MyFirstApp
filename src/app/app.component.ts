0694fe0e8bd96f5fb7be89fb7e9d4222c2cb4677
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-22 13:42:34 +0530

    Pipes introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  title: string = 'Example of pipe expression';
+  Showdate: Date = new Date();
+
+  employees = [
+    {name: 'jason smith', state: 'california', salary: 50000},
+    {name: 'anderson brooke', state: 'new york', salary: 75000},
+    {name: 'alex costa', state: 'texas', salary: 60000},
+    {name: 'sean parker', state: 'pennsylvania', salary: 85000},
+  ];
 }
