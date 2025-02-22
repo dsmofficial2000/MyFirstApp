a9f93896da42d25f03f19903c74687cbabe68f8f
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-22 12:18:32 +0530

    NOT Working host listerner

--- src/app/highlight-element.directive.ts
+++ src/app/highlight-element.directive.ts
@@ -1,17 +1,41 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlightElement]', // selector value is pass on the hosted element
-  standalone: true
+  selector: '[appHighlightElement]',
 })
-export class HighlightElementDirective {
-  
-  @HostBinding('style.backgroundColor') bgColor = 'f2aa4ccff';
+export class HighlightElementDirective implements OnInit {
+  @HostBinding('style.backgroundColor') bgColor?: string;
+  @HostBinding('style.color') textColor?: string;
 
-  constructor() { }
+  colorArray = [
+    '#e94b3cff',
+    '#2d2926ff',
+    '#2a3132',
+    '#343148ff',
+    '#07575b',
+    '#021c1e',
+    '#2a2f33',
+    '#1e1f26'
+  ];
 
+  ngOnInit(): void {
+    this.bgColor = '#f2aa4cff';
+    console.log('Test');
+    this.textColor = '#101820ff';
+  }
+
+  @HostListener('click') onToggle(){
+    this.setRandomColor();
+  }
+
+  private setRandomColor(){
+    const colorPick = Math.floor(Math.random()* this.colorArray.length);
+
+    if(this.bgColor !== this.colorArray[colorPick]){
+      this.bgColor = this.colorArray[colorPick];
+      this.textColor = '#ccc';
+    }else{
+      this.setRandomColor();
+    }
+  }
 }
-
-// The @HostBinding decorator is used within 
-// the custom directive to apply CSS style properties 
-// OR DOM related tasks on the element
