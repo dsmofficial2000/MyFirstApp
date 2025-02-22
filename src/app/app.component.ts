cef6fc6f35853f29f857e6d97f6a16669dc32153
dsmofficial2000 <dsmofficial2000@gmail.com>
2025-02-23 01:33:59 +0530

    Assignment - Page navigation using slice pipe (Part 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,21 +13,77 @@ interface Employee {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'Example of pipe expression';
-  Showdate: Date = new Date();
+  pageStartIndex: number = 0;
+  pageSize: number = 4;
+  endIndex: number = this.pageSize;
+  slicedProducts: { name: string; imageUrl: string }[] = [];
 
-  items = ['ABC', 'DEF', 'GHI', 'JKL'];
-  employees: {name: string, state: string, salary: number }[] =[
-    {name: 'jason smith', state: 'california', salary: 50000},
-    {name: 'anderson brooke', state: 'new york', salary: 75000},
-    {name: 'alex costa', state: 'texas', salary: 60000},
-    {name: 'sean parker', state: 'pennsylvania', salary: 85000},
-  ];
+  ngOnInit() {
+    this.sliceProducts();
+  }
 
-  employees1: Employee[] =[
-    {name: 'jason smith', state: 'california', salary: 50000},
-    {name: 'anderson brooke', state: 'new york', salary: 75000},
-    {name: 'alex costa', state: 'texas', salary: 60000},
-    {name: 'sean parker', state: 'pennsylvania', salary: 85000},
+  sliceProducts() {
+    this.slicedProducts = this.products.slice(this.pageStartIndex, this.endIndex);
+  }
+
+  previousPage(): void {
+    this.pageStartIndex -= this.pageSize;
+    this.endIndex -= this.pageSize;
+  }
+
+  nextPage() : void{
+    this.pageStartIndex += this.pageSize;
+    this.endIndex += this.pageSize;
+  }
+
+  previousPage1(): void {
+    if (this.pageStartIndex - this.pageSize >= 0) {
+      this.pageStartIndex -= this.pageSize;
+      this.endIndex -= this.pageSize;
+      this.sliceProducts();
+    }
+  }
+
+  nextPage1(): void {
+    if (this.endIndex < this.products.length) {
+      this.pageStartIndex += this.pageSize;
+      this.endIndex += this.pageSize;
+      this.sliceProducts();
+    }
+  }
+
+  products: { name: string; imageUrl: string }[] = [
+    {
+      name: 'HTML, CSS, Sass, Bootstrap - Beginner to Expert + Bootcamp',
+      imageUrl: './assets/html-css-tutorials.jpg',
+    },
+    {
+      name: 'JavaScript - Marathon Interview Questions Series',
+      imageUrl: './assets/JS_Course.png',
+    },
+    {
+      name: 'Mastering TypeScript with Marathon Interview Questions',
+      imageUrl: './assets/TypeScript_Course.jpg',
+    },
+    {
+      name: 'Mastering React with Interview Questions, eStore Project',
+      imageUrl: './assets/React_Course.png',
+    },
+    {
+      name: 'Practical Database Guide with RDBMS(MySQL) & NoSQL(MongoDB)',
+      imageUrl: './assets/RDBMS.png',
+    },
+    {
+      name: 'Mastering Angular with Marathon Interview Quesitons',
+      imageUrl: './assets/Angular_Course.jpg',
+    },
+    {
+      name: 'NodeJS - Marathon Interview Questions Series',
+      imageUrl: './assets/NodeJS_Course.jpg',
+    },
+    {
+      name: 'Python - Marathon Interview Questions Series',
+      imageUrl: './assets/Python_Course.jpg',
+    }
   ];
 }
